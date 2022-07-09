19b2dbce2f2f30fac8895e2d8d217e1ede450f73
Davide Basso <davidebasso03@gmail.com>
2022-07-09 15:41:40 +0200

    :bug: fix: fix terminal resize issue

--- src/lib/tools/terminal.svelte
+++ src/lib/tools/terminal.svelte
@@ -8,62 +8,31 @@
 	let terminalElement: HTMLElement;
 	let terminalController: xterm.Terminal;
 	let termFit: fit.FitAddon;
-	$: {
-		if (terminalController) {
-			// ...
-		}
-	}
-	function initalizeXterm() {
+
+	function initializeXterm() {
 		terminalController = new xterm.Terminal();
 		termFit = new fit.FitAddon();
 		terminalController.loadAddon(termFit);
 		terminalController.open(terminalElement);
 		termFit.fit();
 		terminalController.write(
-			'I am a terminal! I am a terminal! I am a terminal! I am a terminal! I am a terminal! I am a terminal! I am a terminal!'
+			'\x1b[32mWelcome to Warp code!Welcome to Warp code!Welcome to Warp code!Welcome to Warp code!Welcome to Warp code!Welcome to Warp code!Welcome to Warp code!Welcome to Warp code!Welcome to Warp code!Welcome to Warp code!Welcome to Warp code!Welcome to Warp code!Welcome to Warp code!\x1b[m\n'
 		);
-		terminalController.onData((e) => {
-			console.log(e);
-		});
 	}
 	onMount(async () => {
-		initalizeXterm();
+		initializeXterm();
 	});
 	function handleTermResize() {
 		if (termFit) {
 			termFit.fit();
-			console.log('Resizing!');
 		}
 	}
 </script>
 
-<!-- Actual terminal -->
 <div id="terminal" bind:this={terminalElement} />
 
-<!-- Resize observer -->
-<div class="absolute top-0 bottom-0 left-0 right-0">
+<div class="absolute top-0 left-0 right-0 bottom-0">
 	<ResizeObserver on:resize={handleTermResize} />
 </div>
 
-<style lang="scss">
-	:global(#terminal) {
-		@apply w-full h-full;
-	}
-
-	:global(#terminal),
-	:global(.xterm .xterm-viewport) {
-		/* width */
-		&::-webkit-scrollbar {
-			@apply w-3;
-		}
-
-		&:hover::-webkit-scrollbar-thumb {
-			@apply bg-primary-600;
-		}
-
-		/* Handle on hover */
-		&::-webkit-scrollbar-thumb:hover {
-			@apply bg-primary-500;
-		}
-	}
-</style>
+<svelte:window on:resize={handleTermResize} />
