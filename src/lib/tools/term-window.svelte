508f0a1a8f35ea5791271c7624e0fc868b205c59
Davide Basso <davidebasso03@gmail.com>
2022-07-09 17:26:48 +0200

    :thread: style: term style

--- src/lib/tools/term-window.svelte
+++ src/lib/tools/term-window.svelte
@@ -4,14 +4,12 @@
 	let windowsCount = 1;
 </script>
 
-<div class="w-full h-full relative">
-	<Terminal />
-	<!-- <div class="flex-grow">
+<div class="w-full h-full relative flex">
+	<!-- <Terminal /> -->
+	<div class="flex-grow">
 		<div class="w-full h-full relative">
 			<Terminal />
 		</div>
 	</div>
-	<div class="w-12 text-primary-100">
-		This is a text
-	</div> -->
+	<div class="w-12 text-primary-100">This is a text</div>
 </div>
--- src/lib/tools/terminal.svelte
+++ src/lib/tools/terminal.svelte
@@ -3,21 +3,32 @@
 	import 'xterm/css/xterm.css';
 	import * as xterm from 'xterm';
 	import * as fit from 'xterm-addon-fit';
-	import ResizeObserver from 'svelte-resize-observer';
+	import { watchResize } from 'svelte-watch-resize';
+	// Tailwind
+	import resolveConfig from 'tailwindcss/resolveConfig';
+	import tailwindConfig from 'tailwind.config.cjs';
+	const fullConfig = resolveConfig(tailwindConfig);
 
 	let terminalElement: HTMLElement;
 	let terminalController: xterm.Terminal;
 	let termFit: fit.FitAddon;
 
 	function initializeXterm() {
-		terminalController = new xterm.Terminal();
+		terminalController = new xterm.Terminal({
+			theme: {
+				background: fullConfig.theme.colors.primary['700'],
+			},
+		});
 		termFit = new fit.FitAddon();
 		terminalController.loadAddon(termFit);
 		terminalController.open(terminalElement);
-		termFit.fit();
-		terminalController.write(
-			'\x1b[32mWelcome to Warp code!Welcome to Warp code!Welcome to Warp code!Welcome to Warp code!Welcome to Warp code!Welcome to Warp code!Welcome to Warp code!Welcome to Warp code!Welcome to Warp code!Welcome to Warp code!Welcome to Warp code!Welcome to Warp code!Welcome to Warp code!\x1b[m\n'
-		);
+		for (let i = 0; i < 20; i++) {
+			terminalController.write('\x1b[32mThis is a working terminal!\x1b[m\n');
+			termFit.fit();
+		}
+		terminalController.onData((e) => {
+			terminalController.write(e);
+		});
 	}
 	onMount(async () => {
 		initializeXterm();
@@ -29,10 +40,28 @@
 	}
 </script>
 
-<div id="terminal" bind:this={terminalElement} />
-
-<div class="absolute top-0 left-0 right-0 bottom-0">
-	<ResizeObserver on:resize={handleTermResize} />
-</div>
+<div
+	id="terminal"
+	bind:this={terminalElement}
+	class="absolute top-0 bottom-0 right-0 left-0"
+	use:watchResize={handleTermResize}
+/>
 
 <svelte:window on:resize={handleTermResize} />
+
+<style lang="scss">
+	:global(.xterm-viewport) {
+		/* width */
+		&::-webkit-scrollbar {
+			@apply w-3;
+		}
+		/* FIXME: */
+		/* Handle on hover */
+		&::-webkit-scrollbar-thumb:hover {
+			@apply bg-primary-400;
+		}
+	}
+	:global(#terminal:hover .xterm-viewport::-webkit-scrollbar-thumb) {
+		@apply bg-primary-600;
+	}
+</style>
